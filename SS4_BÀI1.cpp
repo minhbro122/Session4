9a15097976717a90829a26a0877cee0932589693
minhbro122 <Lynhan.zip.zip.zip@gmail.com>
2024-11-24 20:28:23 +0700

    Add files via upload

---
+++ SS4_BÀI1.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h> 
+int main() {
+	int num; 
+	printf("moi ban nhap vao mot so nguyen");
+	scanf("%d",&num);
+	 if(num <=1) {
+	 	printf("la so nguyen am");
+	 }else if(num>=1){
+	 	printf(" la so nguyen duong");
+	 }
+	
+	
+	
+	return 0; 
+}
